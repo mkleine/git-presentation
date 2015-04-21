6d855a4b2dfa1fd8167b8338e2449c5b9eafaf9d
Moritz Kleine <moritz.kleine@coremedia.com>
2015-04-21 15:31:13 +0200

    write current ref to file so that contents of last commit can be shown via soft reset and iteration starts with hard reset to current state

--- apply-patches.sh
+++ apply-patches.sh
@@ -1,14 +1,23 @@
-HEAD_REF=`git rev-parse HEAD`
+if [ -e ./current_ref.txt ]; then
+  HEAD_REF=`cat ./current_ref.txt`
+  git reset --hard $HEAD_REF
+else 
+  HEAD_REF=`git rev-parse HEAD`
+fi
 echo "head revision is ${HEAD_REF}"
 SOURCE_REF=$1
 if [ -z ${SOURCE_REF+x} ] ; then
   SOURCE_REF=presentation
 fi
 
-for i in `git log ${SOURCE_REF} --cherry-pick --oneline --pretty=format:'%h' ${HEAD_REF}...${SOURCE_REF}`
-  do NEXT_REF=$i 
+for i in `git log ${SOURCE_REF} --cherry-pick --oneline --pretty=format:'%h' ${HEAD_REF}...${SOURCE_REF}`; do 
+  NEXT_REF=$i
+  echo "upcoming revision is ${NEXT_REF}" 
 done
-if [ -z ${NEXT_REF+x} ]; then echo "no more revisions to pick"
+if [ -z ${NEXT_REF+x} ]; then
+  echo "no more revisions to pick"
 else 
   git cherry-pick ${NEXT_REF} && echo "cherry-pick successful"
+  git reset --soft HEAD^
+  echo $NEXT_REF > current_ref.txt
 fi
